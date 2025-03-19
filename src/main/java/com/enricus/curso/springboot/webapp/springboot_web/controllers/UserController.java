b497d02c172fa2e825c8a74bc1ccd8469e718a1d
enrique.amador <enrique.amador@spectrumeffect.com>
2025-03-19 17:59:29 -0600

    adding ModelAttribute

--- src/main/java/com/enricus/curso/springboot/webapp/springboot_web/controllers/UserController.java
+++ src/main/java/com/enricus/curso/springboot/webapp/springboot_web/controllers/UserController.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 
 import com.enricus.curso.springboot.webapp.springboot_web.models.User;
 
@@ -15,7 +16,7 @@ public class UserController {
 
     @GetMapping("/details")
     public String details(Model model){
-        User user = new User("Enrique", "Amador");
+        User user = new User("Enrique", "Amador", "");
         user.setEmail("kike@kike.com");
         model.addAttribute("title", "Hola, perro mundo");
         model.addAttribute("user", user);
@@ -29,14 +30,22 @@ public class UserController {
 
     @GetMapping("/list")
     public String list(ModelMap model){
-        List<User> users = Arrays.asList(
+        // List<User> users = Arrays.asList(
+        //     new User("pepa", "gzz", "a@a.com"), 
+        //     new User("juana", "cubana", "f@a.com"),
+        //     new User("lalo", "landa", null));
+
+        // model.addAttribute("users", users);
+        model.addAttribute("title", "Lista de users");
+        return "list";
+    }
+
+    @ModelAttribute("users") //para pasar datos comunes para las vistas, es decir, para reutilizar datos
+    public List<User> userModel() {
+        return Arrays.asList(
             new User("pepa", "gzz", "a@a.com"), 
             new User("juana", "cubana", "f@a.com"),
             new User("lalo", "landa", null));
-
-        model.addAttribute("users", users);
-        model.addAttribute("title", "Lista de users");
-        return "list";
     }
     
 }
