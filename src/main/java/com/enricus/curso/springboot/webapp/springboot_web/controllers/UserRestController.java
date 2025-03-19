57a0375163e1d34d0e74178bd4b8fd1b70002af6
enrique.amador <enrique.amador@spectrumeffect.com>
2025-03-19 11:29:04 -0600

    adding DTO

--- src/main/java/com/enricus/curso/springboot/webapp/springboot_web/controllers/UserRestController.java
+++ src/main/java/com/enricus/curso/springboot/webapp/springboot_web/controllers/UserRestController.java
@@ -3,13 +3,12 @@ package com.enricus.curso.springboot.webapp.springboot_web.controllers;
 import java.util.HashMap;
 import java.util.Map;
 
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.enricus.curso.springboot.webapp.springboot_web.Hijo;
 import com.enricus.curso.springboot.webapp.springboot_web.User;
+import com.enricus.curso.springboot.webapp.springboot_web.models.dto.UserDto;
 // import org.springframework.web.bind.annotation.ResponseBody;
 
 // @Controller + @ResponseBody
@@ -17,8 +16,19 @@ import com.enricus.curso.springboot.webapp.springboot_web.User;
 @RequestMapping("/api") //ruta base de primer nivel
 public class UserRestController {
 
-    @GetMapping("/details") //@RequestMapping(path="/details", method = RequestMethod.GET)
-    public Map<String, Object> details(Model model){
+    @GetMapping(path="/details") //@RequestMapping(path="/details", method = RequestMethod.GET)
+    public UserDto details(){
+        User user = new User("Enrique", "Amador");
+        
+        UserDto userDto = new UserDto();
+        userDto.setUser(user);
+        userDto.setTitle("Hola, mundillo");
+
+        return userDto; //devolviendo DTO
+    }
+
+    @GetMapping(path="/details-map") //@RequestMapping(path="/details", method = RequestMethod.GET)
+    public Map<String, Object> detailsMap(){
         User user = new User("Enrique", "Amador");
 
         Map<String, Object> body = new HashMap<>();
@@ -26,11 +36,7 @@ public class UserRestController {
         body.put("title", "Hola, perro mundo");
         body.put("user", user);
 
-        // model.put("title", "Hola, perro mundo con map");
-        // model.put("name", "Enrique");
-        // model.put("lastname", "Amador");
-
-        return body; //nombre de la plantilla en resources/templates
+        return body; //devolviendo map
     }
     
 }
