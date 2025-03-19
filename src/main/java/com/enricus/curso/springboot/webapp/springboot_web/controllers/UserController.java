c2ec1fe260482ec812a287acaaa2be8dff486f13
enrique.amador <enrique.amador@spectrumeffect.com>
2025-03-18 23:30:45 -0600

    returning an object to the html and to json api rest

--- src/main/java/com/enricus/curso/springboot/webapp/springboot_web/controllers/UserController.java
+++ src/main/java/com/enricus/curso/springboot/webapp/springboot_web/controllers/UserController.java
@@ -6,15 +6,17 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import com.enricus.curso.springboot.webapp.springboot_web.User;
+
 @Controller
 public class UserController {
 
     @GetMapping("/details")
     public String details(Model model){
+        User user = new User("Enrique", "Amador");
 
         model.addAttribute("title", "Hola, perro mundo");
-        model.addAttribute("name", "Enrique");
-        model.addAttribute("lastname", "Amador");
+        model.addAttribute("user", user);
 
         // model.put("title", "Hola, perro mundo con map");
         // model.put("name", "Enrique");
--- src/main/java/com/enricus/curso/springboot/webapp/springboot_web/controllers/UserRestController.java
+++ src/main/java/com/enricus/curso/springboot/webapp/springboot_web/controllers/UserRestController.java
@@ -5,19 +5,26 @@ import java.util.Map;
 
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.enricus.curso.springboot.webapp.springboot_web.Hijo;
+import com.enricus.curso.springboot.webapp.springboot_web.User;
+// import org.springframework.web.bind.annotation.ResponseBody;
+
+// @Controller + @ResponseBody
 @RestController
+@RequestMapping("/api") //ruta base de primer nivel
 public class UserRestController {
 
-    @GetMapping("/details2")
+    @GetMapping("/details") //@RequestMapping(path="/details", method = RequestMethod.GET)
     public Map<String, Object> details(Model model){
+        User user = new User("Enrique", "Amador");
 
         Map<String, Object> body = new HashMap<>();
 
         body.put("title", "Hola, perro mundo");
-        body.put("name", "Enrique");
-        body.put("lastname", "Amador");
+        body.put("user", user);
 
         // model.put("title", "Hola, perro mundo con map");
         // model.put("name", "Enrique");
