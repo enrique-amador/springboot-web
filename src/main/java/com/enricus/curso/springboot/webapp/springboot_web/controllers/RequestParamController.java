f5641de35bd3e9a1ffcc15d79df186c69075e7f1
enrique.amador <enrique.amador@spectrumeffect.com>
2025-03-19 18:13:34 -0600

    How to get a param from url: @RequestParam

---
+++ src/main/java/com/enricus/curso/springboot/webapp/springboot_web/controllers/RequestParamController.java
@@ -0,0 +1,21 @@
+package com.enricus.curso.springboot.webapp.springboot_web.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.enricus.curso.springboot.webapp.springboot_web.models.dto.ParamsDto;
+
+@RestController
+@RequestMapping("/api/params")
+public class RequestParamController {
+
+    @GetMapping("/foo")
+    public ParamsDto foo(@RequestParam(required=false, defaultValue="When message doens't come") String message){//message has to come in the URL
+        ParamsDto paramDto = new ParamsDto();
+        paramDto.setMessage(message);
+        return paramDto;
+    }
+
+}
---
+++ src/main/java/com/enricus/curso/springboot/webapp/springboot_web/models/dto/ParamsDto.java
@@ -0,0 +1,15 @@
+package com.enricus.curso.springboot.webapp.springboot_web.models.dto;
+
+public class ParamsDto {
+
+    private String message;
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+}
