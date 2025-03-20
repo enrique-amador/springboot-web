14a3bb85cf003cb7287da243c520844e10121291
enrique.amador <enrique.amador@spectrumeffect.com>
2025-03-19 21:36:03 -0600

    handling a POST request with postman. also adding handling of several PathVariable

--- src/main/java/com/enricus/curso/springboot/webapp/springboot_web/controllers/PathVariableController.java
+++ src/main/java/com/enricus/curso/springboot/webapp/springboot_web/controllers/PathVariableController.java
@@ -1,10 +1,16 @@
 package com.enricus.curso.springboot.webapp.springboot_web.controllers;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.enricus.curso.springboot.webapp.springboot_web.models.User;
 import com.enricus.curso.springboot.webapp.springboot_web.models.dto.ParamsDto;
 
 @RestController
@@ -16,5 +22,19 @@ public class PathVariableController {
         ParamsDto param = new ParamsDto();
         param.setMessage(message);
         return param;
+    }
+
+    @GetMapping("/mix/{product}/{id}")
+    public Map<String, Object>  mix(@PathVariable String product, @PathVariable Long id){
+        Map<String, Object> map = new HashMap<>();
+        map.put("product", product);
+        map.put("id", id);
+        return map;
+    }
+
+    @PostMapping("/create")
+    public User create(@RequestBody User user){
+        user.setName(user.getName().toUpperCase());
+        return user;
     }
 }
