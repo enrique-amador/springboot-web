fe2a44c97a9253ff437c4f17e2f69630e98d71af
enrique.amador <enrique.amador@spectrumeffect.com>
2025-03-24 15:14:07 -0600

    adding config variables to the application.properties and injecting them

--- src/main/java/com/enricus/curso/springboot/webapp/springboot_web/controllers/PathVariableController.java
+++ src/main/java/com/enricus/curso/springboot/webapp/springboot_web/controllers/PathVariableController.java
@@ -3,6 +3,7 @@ package com.enricus.curso.springboot.webapp.springboot_web.controllers;
 import java.util.HashMap;
 import java.util.Map;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -16,6 +17,19 @@ import com.enricus.curso.springboot.webapp.springboot_web.models.dto.ParamsDto;
 @RestController
 @RequestMapping("/api/var")
 public class PathVariableController {
+
+
+    @Value("${config.code}")
+    private Long code;
+
+    @Value("${config.username}")
+    private String username;
+    
+    // @Value("${config.message}")
+    // private String message;
+    
+    @Value("${config.listOfValues}")
+    private String[] listOfValues;
 
     @GetMapping("/baz/{message}")
     public ParamsDto baz(@PathVariable() String message){ //a;ways has to come
@@ -36,5 +50,16 @@ public class PathVariableController {
     public User create(@RequestBody User user){
         user.setName(user.getName().toUpperCase());
         return user;
+    }
+
+    @GetMapping("/values")
+    public Map<String, Object> values(@Value("${config.message}") String message){
+        Map<String, Object> json = new HashMap<>();
+        json.put("code", code);
+        json.put("username", username);
+        json.put("message", message);
+        json.put("listOfValues", listOfValues);
+
+        return json;
     }
 }
